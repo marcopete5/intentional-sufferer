7d82e23cf39d62b6c9737f1726131c14e8c1f329
marcopete5 <mdabomb5@gmail.com>
2018-06-06 11:00:47 -0600

    added wheel

--- src/Home.js
+++ src/Home.js
@@ -2,17 +2,50 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { addOption } from './Redux/options';
 import { addToHistory } from './Redux/history';
+import PieChart from 'react-svg-piechart';
+import axios from 'axios';
+
 
 class Home extends Component {
     constructor() {
         super();
 
         this.state = {
-            selected: 'Spin the Wheel',
+            selected: '',
             input: '',
             adding: false,
+            deg: 0,
+            data: [
+                { title: "Data 1", value: 30, color: "black" },
+                { title: "Data 2", value: 30, color: "#f5f5f8a6" },
+                { title: "Data 3", value: 30, color: "black" },
+                { title: "Data 4", value: 30, color: "#f5f5f8a6" },
+                { title: "Data 5", value: 30, color: "black" },
+                { title: "Data 5", value: 30, color: "#f5f5f8a6" },
+                { title: "Data 5", value: 30, color: "black" },
+                { title: "Data 5", value: 30, color: "#f5f5f8a6" },
+                { title: "Data 5", value: 30, color: "black" },
+                { title: "Data 5", value: 30, color: "#f5f5f8a6" }
+            ]
         }
     }
+
+    // componentWillMount = () => {
+    //     const data = this.state.data;
+    //     for(var i = 0; i < data.length; i++){
+    //         (function(x){
+    //             axios.get('http://www.colr.org/json/color/random').then(response => {
+    //                 data[x].color = '#' + response.data.new_color;
+    //                 this.setState({data});
+    //             }).catch((error) => {
+    //                 console.log(error);
+    //             });
+    //         })(i)
+    //     }
+    // }
+        
+
+    
 
     spinWheel = () => {
         let { history, options } = this.props;
@@ -22,11 +55,17 @@ class Home extends Component {
             console.log('copy found')
             random = options[Math.floor(Math.random() * options.length)]
         }
-        this.setState({ selected: random }, () => {
-            console.log(this.state);
-            if (this.state.selected !== 'Spin the Wheel') {
-                this.props.addToHistory(this.state.selected)
-            }
+        this.setState(prevState => { 
+            return { 
+                selected: '',
+                deg: prevState.deg + Math.floor(Math.random()* 360)+720
+            }}, () => {
+            setTimeout(()=>{
+                this.setState({selected: random})
+                if (this.state.selected !== 'Spin the Wheel' && this.state.selected !== '') {
+                    this.props.addToHistory(this.state.selected)
+                }
+            },3010)
         })
     }
 
@@ -42,13 +81,13 @@ class Home extends Component {
     handleChange = e => {
         this.setState({ input: e.target.value })
     }
-
     render() {
+
         return (
             <div className='homeContainer'>
                 <div className="spin">
+                    <button onClick={this.spinWheel} id='spinBtn'>SPIN</button>
                     <h1>{this.state.selected}</h1>
-                    <button onClick={this.spinWheel}>SPIN</button>
                 </div>
 
                 {
@@ -63,7 +102,16 @@ class Home extends Component {
                             <button onClick={this.addSuck}>ADD</button>
                         </div>
                 }
+                <img className='spinArrow' src="https://image.flaticon.com/icons/svg/53/53677.svg" alt=""/>
+                <div className='pieDiv' style={{ transform: `rotate(${this.state.deg}deg)` }}>
+                    <PieChart
+                        data={this.state.data}
+                        expandOnHover
+                    />
+                </div>
+
             </div>
+
         );
     }
 }
--- src/Redux/options.js
+++ src/Redux/options.js
@@ -5,7 +5,7 @@ export function addOption(option) {
     }
 }
 
-const reducer = (prevState = ['Fast for 12 hours', 'Zero sugar/carbs all day', 'No meat all week', 'Say nothing all day except in answer to questions', 'Leave your phone at home all day', 'Give the first person you see after leaving your house $10', 'Bike 30+ miles', 'Run/Walk 10 miles', 'No television shows this week', 'Talk to a stranger for 5 min'], action) => {
+const reducer = (prevState = ['Fast for 12 hours', 'Get rejected today', 'Sit in silence without moving for 30 min', 'Zero sugar/carbs all day', 'No meat all week', 'Say nothing all day except in answer to questions', 'Leave your phone at home all day', 'Give the first person you see after leaving your house $10', 'Bike 30+ miles', 'Run/Walk 10 miles', 'No television shows this week', 'Talk to a stranger for 5 min'], action) => {
     switch (action.type) {
         case 'ADD_OPTION':
             return [...prevState, action.option]
